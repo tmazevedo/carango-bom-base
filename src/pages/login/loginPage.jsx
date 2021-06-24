7d8130cdcaa66774896fc42783f377096a29c2ae
Adriano Groff <adrianokgroff@gmail.com>
2021-06-24 17:07:30 -0300

    feat: add login page

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 const LoginPage = () => {
-  return <>login page</>;
+  return (
+    <>
+      <form autoComplete="off">
+        <TextField
+          label="Usuário"
+          required
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          type="text"
+        />
+        <TextField
+          label="Senha"
+          required
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          type="password"
+        />
+      </form>
+      <div>
+        <Button className="action-item">Recuperar senha</Button>
+      </div>
+      <div className="action-itens">
+        <Button className="action-item" variant="outlined" color="primary">
+          Entrar
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
