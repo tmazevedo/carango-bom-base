3a64c85078cb8b2757e95f0c514955447d9c39d6
Vitor Falcão <vitorfhc@protonmail.com>
2021-07-01 18:15:05 +0000

    fix: test for page404

--- src/tests/page404/page404.test.jsx
+++ src/tests/page404/page404.test.jsx
@@ -5,7 +5,7 @@ import Page404 from '../../pages/page404/Page404';
 describe('When Create 404 Page Component', () => {
     it('should render Page not found', () => {
         render(<Page404 />);
-        const element = screen.getAllByText('Página não encontrada');
+        const element = screen.getByText('Página não encontrada');
         expect(element).toBeInTheDocument();
     });
 
