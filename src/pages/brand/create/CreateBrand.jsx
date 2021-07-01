7ab947605ab4909a766ec8a4ecce90de50805e4e
Vitor Falcão <vitorfhc@protonmail.com>
2021-07-01 19:22:43 +0000

    fix: change createBrand to use Form component and remove brand tests

--- src/pages/brand/create/CreateBrand.jsx
+++ src/pages/brand/create/CreateBrand.jsx
@@ -1,58 +1,16 @@
-import { Button, TextField, makeStyles } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
+import Form from '../../../components/form';
 
-import { Link } from 'react-router-dom';
-
-const useStyles = makeStyles(() => ({
-  fab: {
-    position: 'absolute',
-    bottom: '100px',
-    right: '100px',
-  },
-  actionsToolbar: {
-    float: 'right',
-  },
-  actions: {
-    top: '10px',
-  },
-}));
-
-const CreateBrand = () => {
-  const [brand, setBrand] = useState('');
-  const classes = useStyles();
-
-  return (
-    <>
-      <Link className="link" to="/marcas">
-        <Button variant="outlined" type="submit">
-          Voltar
-        </Button>
-      </Link>
-
-      <form>
-        <TextField
-          value={brand}
-          onChange={(evt) => setBrand(evt.target.value)}
-          // onBlur={validarCampos}
-          // helperText={erros.marca.texto}
-          // error={!erros.marca.valido}
-          name="brand"
-          id="brand"
-          label="Marca"
-          type="text"
-          variant="outlined"
-          fullWidth
-          required
-          margin="normal"
-        />
-        <div className={classes.actionsToolbar}>
-          <Button variant="outlined" color="primary" type="submit">
-            Salvar
-          </Button>
-        </div>
-      </form>
-    </>
-  );
-};
+function CreateBrand() {
+  <Form
+    fields={[
+      { name: 'brand', label: 'Marca', required: true },
+    ]}
+    mainButton={{
+      text: 'Salvar',
+      onSubmit: () => { },
+    }}
+  />;
+}
 
 export default CreateBrand;
--- src/tests/brand/brandPage.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen, RenderResult } from '@testing-library/react';
-import BrandPage from '../../pages/brand/BrandPage';
-
-describe('When create a Brand Component',()=>{
-    describe('and the button renders',()=>{
-        
-    })
-})
