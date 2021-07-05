fa5b84f3f93053608267f08877d1281b106ee02e
Thiago Machado de Azevedo <thiago_machado_azevedo@hotmail.com>
2021-07-05 11:06:23 -0300

    fear: create a dashboard service

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import DashboardService from '../../services/DashboardService';
 
 const useStyles = makeStyles({
   root: {
@@ -25,83 +26,67 @@ const useStyles = makeStyles({
 });
 
 const DashboardPage = () => {
+  const [dashboardList, setDashboardList] = useState([]);
+  const [totalCars, setTotalCars] = useState('');
+
+  useEffect(() => {
+
+    function countCars(data) {
+      let element = 0;
+      for (let index = 0; index < data.length; index++) {
+        element = data[index].count + element;
+      }
+      return element;
+    }
+
+    async function loadDashboard() {
+      await DashboardService.List()
+        .then(data => {
+          setDashboardList(data);
+          setTotalCars(countCars(data));
+        });
+
+    }
+    loadDashboard();
+
+  }, [totalCars]);
+
   const classes = useStyles();
 
   return (
     <>
       <Box bgcolor="info.main" color="info.contrastText" p={2}>
-        <strong>485</strong>
+        <strong>{totalCars}</strong>
         {' '}
         veículos encontrados
       </Box>
+      {
+        dashboardList.map((value) => {
+          return (
+            <div style={{ padding: '0 12px', margin: '40px 0' }}>
+              <Grid container spacing={5}>
+                <Grid container item lg={4}>
+                  <Card className={classes.root} variant="outlined">
+                    <CardContent>
+                      <Typography variant="h5" component="h2">
+                        {value.brand}
+                      </Typography>
+                      <Typography className={classes.pos} color="textSecondary">
+                        {value.count}
+                      </Typography>
+                      <Typography variant="body2" component="p">
+                        R$
+                        {value.totalPrice}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+            </div>
+          );
+        })
+      }
 
-      <div style={{ padding: '0 12px', margin: '40px 0' }}>
-        <Grid container spacing={5}>
-          <Grid container item lg={4}>
-            <Card className={classes.root} variant="outlined">
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  Hyundai
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  47 veículos
-                </Typography>
-                <Typography variant="body2" component="p">
-                  R$ 249.300,00
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid container item lg={4}>
-            <Card className={classes.root} variant="outlined">
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  Fiat
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  5 veículos
-                </Typography>
-                <Typography variant="body2" component="p">
-                  R$ 90.000,00
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid container item lg={4}>
-            <Card className={classes.root} variant="outlined">
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  Volkswagen
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  985 veículos
-                </Typography>
-                <Typography variant="body2" component="p">
-                  R$ 559.580,00
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid container item md={4}>
-            <Card className={classes.root} variant="outlined">
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  Renault
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  11 veículos
-                </Typography>
-                <Typography variant="body2" component="p">
-                  R$ 99.000,00
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </div>
     </>
   );
 };
---
+++ src/services/DashboardService.js
@@ -0,0 +1,17 @@
+const token = localStorage.getItem('token');
+
+const DashboardService = {
+    List() {
+        return fetch(process.env.REACT_APP_SERVER_URL + 'dashboard/cars-by-brand', {
+            method: 'GET',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        }).then(r => r.json());
+    },
+};
+
+export default DashboardService;
