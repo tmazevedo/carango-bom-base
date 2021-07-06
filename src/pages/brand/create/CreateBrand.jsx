b6440eeaae36cf744cb9eac76a95dc96f49f5d78
Thiago Machado de Azevedo <thiago_machado_azevedo@hotmail.com>
2021-07-06 15:20:16 -0300

    change brand

--- src/pages/brand/create/CreateBrand.jsx
+++ src/pages/brand/create/CreateBrand.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Form from '../../../components/form';
 import BrandService from '../../../services/BrandService';
 
 const CreateBrand = () => {
   const history = useHistory();
+  const { id } = useParams();
+  const [brandFind, setBrandFind] = useState('');
+  const [loading, setLoading] = useState(true);
 
   function onSubmit(value) {
-    BrandService.Save(value);
+    if (id) {
+      BrandService.UpdateUser(JSON.stringify(value), id);
+      history.goBack();
+    } else {
+      BrandService.Save(value);
+      history.goBack();
+    }
     history.goBack();
   }
 
+  const findBrand = () => {
+    if (id) {
+      BrandService.FindById(id).then(dataFind => {
+        const objectVehicle = {
+          'name': dataFind.name,
+        };
+        setBrandFind(objectVehicle);
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
+    }
+  };
+
+  useEffect(async () => {
+    findBrand();
+  }, []);
+
   return (
-    <Form
-      fields={[
-        { name: 'name', label: 'Marca', required: true },
-      ]}
-      mainButton={{
-        text: 'Salvar',
-        onSubmit,
-      }}
-    />
+    loading ? <CircularProgress />
+      : <Form
+        fields={[
+          { name: 'name', label: 'Marca', required: true },
+        ]}
+        mainButton={{
+          text: 'Salvar',
+          onSubmit,
+        }}
+        value={brandFind}
+      />
   );
 };
 
--- src/services/BrandService.js
+++ src/services/BrandService.js
@@ -24,6 +24,29 @@ const BrandService = {
             body: JSON.stringify(objectToSave),
         }).then(r => r.json());
     },
+    FindById(id) {
+        return fetch(process.env.REACT_APP_SERVER_URL + 'brands/' + id, {
+            method: 'GET',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        }).then(r => r.json());
+    },
+    UpdateUser(objectToChange, id) {
+        return fetch(process.env.REACT_APP_SERVER_URL + 'brands/' + id, {
+            method: 'PUT',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: objectToChange,
+        }).then(r => r.json());
+    },
 };
 
 export default BrandService;
