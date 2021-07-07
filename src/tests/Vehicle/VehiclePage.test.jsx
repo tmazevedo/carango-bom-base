93ef56fff097affb3f8f0001fe7b7251461235d9
Eduardo Henrique Kenji Shibukawa <eduardo.shibukawa@MacBook-Pro-de-Eduardo.local>
2021-07-07 11:41:26 -0300

    Fixed VehiclePage.test

--- src/tests/Vehicle/VehiclePage.test.jsx
+++ src/tests/Vehicle/VehiclePage.test.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import VehiclePage from '../../pages/vehicle/VehiclePage';
 
 describe('When I create a Vehicle Component', () => {
   describe('and the button renders', () => {
     it('should expect Novo button', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Novo')).toBeInTheDocument();
     });
     it('should expect Alterar button', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Alterar')).toBeInTheDocument();
     });
     it('should expect Excluir button', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Excluir')).toBeInTheDocument();
     });
   });
   describe('and the Columns Table', () => {
     it('should expect Marca Column', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Marca')).toBeInTheDocument();
     });
     it('should expect Modelo Column', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Modelo')).toBeInTheDocument();
     });
     it('should expect Ano Column', () => {
-      render(<VehiclePage />);
+      render(<VehiclePage />, { wrapper: MemoryRouter });
       expect(screen.getByText('Ano')).toBeInTheDocument();
     });
   });
 });
-
-// TODO Test a remove Method when is implemmented
