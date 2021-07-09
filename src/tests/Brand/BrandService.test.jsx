3b87824430a6a9c75be037d1a708bf0227578753
Thiago Machado de Azevedo <thiago_machado_azevedo@hotmail.com>
2021-07-09 12:23:35 -0300

    create a brand service test

---
+++ src/tests/Brand/BrandService.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import BrandService from '../../services/BrandService';
+
+
+jest.mock('../../services/BrandService');
+
+describe('should get all brands from backend', () => {
+    const arr = BrandService.List.mockImplementation(() => Promise.resolve(
+        { id: 1, name: 'Ford' },
+        { id: 2, name: 'Fiat' },
+        { id: 3, name: 'Volvo' },
+        { id: 4, name: 'Porshe' },
+    ));
+
+    it('List from backend is not empty', () => {
+        if (arr.length > 0) {
+            expect(arr).toBe(expect.arrayContaining(expect.toMatchObject({ id: expect.any(Number), name: expect.any(String) })));
+        }
+    });
+});
