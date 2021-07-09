97fd6444661ea56f2384f69f2d53a4ba224ec427
Vitor Falcão <vitorfhc@protonmail.com>
2021-07-09 19:15:26 +0000

    refactor dashboard initial tests

---
+++ src/tests/Dashboard/constants.js
@@ -0,0 +1,19 @@
+const dashboardList = [
+  {
+    brand: 'Fiat',
+    totalPrice: 5000,
+    count: 6,
+  },
+  {
+    brand: 'Toyota',
+    totalPrice: 6000,
+    count: 7,
+  },
+  {
+    brand: 'Ford',
+    totalPrice: 120000,
+    count: 3,
+  },
+];
+
+export default dashboardList;
