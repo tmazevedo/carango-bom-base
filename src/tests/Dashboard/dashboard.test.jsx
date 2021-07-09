a408de15e75d64bf9005c0dc0deb48a66fb874f7
Thiago Machado de Azevedo <thiago_machado_azevedo@hotmail.com>
2021-07-09 11:01:16 -0300

    Create a dashboard test

--- src/tests/Dashboard/dashboard.test.jsx
+++ src/tests/Dashboard/dashboard.test.jsx
@@ -7,27 +7,40 @@ import DashboardPage from '../../pages/dashboard/DashboardPage';
 jest.mock('../../services/DashboardService');
 
 describe('When I create a Dashboard Component', () => {
-    beforeEach(async () => {
-        DashboardService.List.mockImplementation(() =>
-            [
-                {
-                    "brand": "Fiat",
-                    "totalPrice": 1231295358.00,
-                    "count": 6
-                },
-            ],
-        );
-        render(
-            <DashboardPage />,
-            { wrapper: MemoryRouter },
-        );
+  beforeEach(async () => {
+    DashboardService.List.mockImplementation(() => [
+      {
+        brand: 'Fiat',
+        totalPrice: 1231295358.00,
+        count: 6,
+      },
+    ]);
+    render(
+      <DashboardPage />,
+      { wrapper: MemoryRouter },
+    );
+  });
+  describe('and the cards render', () => {
+    it('should expect count of cars', () => {
+      expect(screen.findByText('veículos encontrados'));
     });
-    describe('and the cards render', () => {
-        it('should expect count of cars', () => {
-            expect(screen.findByText('veículos encontrados'));
-        });
-        it('should expect Brand Name', () => {
-            expect(screen.findByText('Fiat'));
-        });
+    it('should expect Brand Name', () => {
+      expect(screen.findByText('Fiat'));
     });
-})
+  });
+});
+
+describe('should render all components from sidebar', () => {
+  it('should expect Marcas button', () => {
+    expect(screen.findByText('Marcas'));
+  });
+  it('should expect Veiculos button', () => {
+    expect(screen.findByText('Veículos'));
+  });
+  it('should expect Usuarios button', () => {
+    expect(screen.findByText('Usuário'));
+  });
+  it('should expect Sair button', () => {
+    expect(screen.findByText('Sair'));
+  });
+});
